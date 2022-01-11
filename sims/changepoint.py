48178cae633258aeccd87186e35fca8aca97a550
Asher Spector <amspector100@gmail.com>
2022-01-10 22:51:24 -0800

    change min_p0

--- sims/changepoint.py
+++ sims/changepoint.py
@@ -62,7 +62,7 @@ def single_seed_sim(
 	for well_specified in args.get('well_specified', [False, True]):
 		if well_specified:
 			p0 = 1 - sparsity
-			min_p0 = 0.8
+			min_p0 = 0.9
 			tau2 = coeff_size
 			sigma2 = 1
 			update = False
