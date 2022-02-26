ace98746b3e7c093363a4b3b1e2fbf6d504f77da
amspector100 <amspector100@gmail.com>
2022-02-26 02:25:02 -0500

    sims modifications

--- sims/changepoint.sh
+++ sims/changepoint.sh
@@ -4,21 +4,27 @@
 #SBATCH -n 32 #Number of cores
 #SBATCH -t 7-00:00 #Runtime in (D-HH:MM)
 #SBATCH --mem-per-cpu=4000 #Memory per cpu in MB (see also --mem)
-#SBATCH -o ../output/cp2_%j.out #File to which standard out will be written
-#SBATCH -e ../output/cp2_%j.err #File to which standard err will be written
+#SBATCH -o ../output/cp300_%j.out #File to which standard out will be written
+#SBATCH -e ../output/cp300_%j.err #File to which standard err will be written
 #SBATCH --mail-type=ALL #Type of email notification- BEGIN,END,FAIL,ALL
 
-NREPS=256
+NREPS=128
 NPROCESSES=32
 
 CP_ARGS="
         --reps $NREPS
         --num_processes $NPROCESSES
-        --p [200]
-        --coeff_size [1,5,9,13,17]
-	--nsample [20000]
-	--well_specified [False]
-        --sparsity [0.01,0.05,0.1]
+        --p [300]
+        --coeff_size [5,9,13]
+	--nsample [10000]
+	--chains 10
+	--bsize 5
+	--well_specified [True,False]
+        --sparsity [0.03]
+	--spacing [2,5,8,11,14,17,20]
+	--reversion_prob [0]
+	--run_lss 1
+	--run_bcp 1
 "
 # Install python/anaconda, activate base environment
 module purge
@@ -36,6 +42,3 @@ export R_LIBS_USER=$HOME/apps/R_4.0.2:$R_LIBS_USERi
 python3.9 changepoint.py $CP_ARGS
 
 module purge
-
-
-
--- sims/glms.sh
+++ sims/glms.sh
@@ -4,8 +4,8 @@
 #SBATCH -n 32 #Number of cores
 #SBATCH -t 7-00:00 #Runtime in (D-HH:MM)
 #SBATCH --mem-per-cpu=4000 #Memory per cpu in MB (see also --mem)
-#SBATCH -o ../output/glm_bin3_%j.out #File to which standard out will be written
-#SBATCH -e ../output/glm_bin3_%j.err #File to which standard err will be written
+#SBATCH -o ../output/glm_pk5hd_%j.out #File to which standard out will be written
+#SBATCH -e ../output/glm_pk5hd_%j.err #File to which standard err will be written
 #SBATCH --mail-type=ALL #Type of email notification- BEGIN,END,FAIL,ALL
 
 NREPS=128
@@ -16,7 +16,7 @@ VP_ARGS="
         --num_processes $NPROCESSES
         --p [500,1000,1500,2000,3000,5000,7500,10000]
         --coeff_size 1
-        --kappa [0.5]
+        --kappa [0.1]
         --method [ar1]
         --y_dist [gaussian]
 	--nsample [1000,10000]
@@ -24,75 +24,72 @@ VP_ARGS="
         --max_corr 0.9999
         --k [2]
         --sparsity [0.05]
+	--run_crt [0]
 	--run_dap [0]
 "
 
-# All kappa: start0.1end1numvals9 and then [1.125, 1.25, 1.375,1.5,1.625,1.75,1.875,2]
+# All kappa: [0.1,0.2,0.35,0.5,0.75,1,1.25,1.5,1.75,2.0]
+# We do not run kappa > 1 for the sparser settings
 # All sparsity: [0.01,0.05,0.1,0.2]
 # y_dist [gaussian,probit]
 # run dap only for sparsity == 0.01
 
-HIGH_ARGS="
+HIGH_ARGS_COMMON="
         --reps $NREPS
         --num_processes $NPROCESSES
         --p 1000
 	--coeff_size 0.5
-        --kappa start0.1end1numvals9 
-        --method [ar1]
+        --kappa [1]
+        --covmethod [ark]
         --y_dist [gaussian]
-	--max_corr 0.9999
+	--max_corr 0.99
 	--k [5]
-	--sparsity [0.01]
-	--run_dap [1]
 	--nsample 5000
+	--chains 10
+	--bsize 5
+	--screen 0
 "
 
-# Bin low-dim setting
+HIGH_P1="${HIGH_ARGS_COMMON}
+	--sparsity [0.01]
+	--run_dap [0]	
+	--run_crt [1]
+	--levels 8
+"
+
+HIGH_P2="${HIGH_ARGS_COMMON}
+	--sparsity [0.1,0.2]
+	--run_dap [0]
+	--run_crt [1]
+	--levels 8
+"
+
+# Bin setting
+
+# Original setting: coef_size [1] kappa [0.1,0.5,1.25,2,3.5,5,6.5]
+
+# In originalsetting
+# Nsample: 5000
+# chains: 10
+
 BIN_ARGS="
 	--reps $NREPS
 	--num_processes $NPROCESSES
 	--p 400
 	--coeff_size [1]
-	--kappa [0.5,2,3.5,5]
+	--kappa [0.1,1.25]
 	--method ar1
 	--y_dist [probit]
 	--well_specified [True,False]
-	--k [1]
-	--max_corr 0.9999
+	--k [5]
+	--chains 10
+	--max_corr 0.99
+	--nsample 5000
 	--sparsity [0.01]
+	--screen [0]
+	--run_crt [1]
 	--run_dap [1]
-	--nsample 3000
-"
-
-# Bin high-dim setting
-BIN_HD_ARGS="
-        --reps $NREPS
-        --num_processes $NPROCESSES
-        --p 400
-        --coeff_size [7.5,10]
-        --kappa [0.5]
-        --method ar1
-        --y_dist [probit]
-        --well_specified [True,False]
-        --k [1]
-        --sparsity [0.01,0.03,0.05]
-        --run_dap [1]
-        --nsample 2500
-"
-
-
-
-LOW_ARGS="
-	--reps $NREPS
-	--num_processes $NPROCESSES
-	--p 400
-	--coeff_size 0.01
-	--kappa start2end20numvals10
-	--method [ar1]
-	--y_dist [gaussian,binomial]
-	--max_corr 0.9999
-	--k [1,3,5]
-	--sparsity [0.01,0.03,0.05,0.1]
+	--levels 8
 "
 
 # Install python/anaconda, activate base environment
@@ -108,9 +105,8 @@ module load R/4.0.2-fasrc01
 # Load packages
 export R_LIBS_USER=$HOME/apps/R_4.0.2:$R_LIBS_USERi
 
-#python3.9 glms.py $HIGH_ARGS
+#python3.9 glms.py $HIGH_P1
 python3.9 glms.py $BIN_ARGS
-#python3.9 glms.py $BIN_HD_ARGS
 #python3.9 glms.py $VP_ARGS
 
 module purge
